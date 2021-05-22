522b2ab6940dbea98b0c1ac0855926902a000245
Zain Tariq <zaintariq981@gmail.com>
2021-05-22 05:28:48 +0500

    Updates

---
+++ precache-manifest.c6e4cc7c0479c631b441387cb336f507.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "810d3a78b3879db3a877f954b8415fc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c4008cee9c57761f8bf",
+    "url": "/static/css/3.3a26ebde.chunk.css"
+  },
+  {
+    "revision": "35d83ad062ee3255017a",
+    "url": "/static/css/4.ea3dbbf2.chunk.css"
+  },
+  {
+    "revision": "6004e1744481ea44893e",
+    "url": "/static/css/main.e6dc66e5.chunk.css"
+  },
+  {
+    "revision": "a7a46d5c82bf6f901a4e",
+    "url": "/static/js/2.92523250.chunk.js"
+  },
+  {
+    "revision": "b9941b26ce7f87a25d1a3bc0d5ae97bc",
+    "url": "/static/js/2.92523250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c4008cee9c57761f8bf",
+    "url": "/static/js/3.1c933d17.chunk.js"
+  },
+  {
+    "revision": "35d83ad062ee3255017a",
+    "url": "/static/js/4.96838317.chunk.js"
+  },
+  {
+    "revision": "6004e1744481ea44893e",
+    "url": "/static/js/main.d76a7dab.chunk.js"
+  },
+  {
+    "revision": "a097927d240c16261b9d",
+    "url": "/static/js/runtime-main.c2cc74b2.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "9014b234123db8aaab3b333cc613a9b3",
+    "url": "/static/media/scholarship.9014b234.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "407d616c420f4781b92d6f08cf553c6e",
+    "url": "/static/media/ucplogo.407d616c.png"
+  }
+]);
